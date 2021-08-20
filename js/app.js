ac3e5e456cb866b8f79bd9d0addd601cca5454de
taseenbappi <taseen.bappi@gmail.com>
2021-08-20 12:48:36 +0600

    dom manupulation added

---
+++ js/app.js
@@ -0,0 +1,16 @@
+const sectionTitle = document.getElementsByClassName('title');
+for (let title of sectionTitle) {
+    title.style.color = 'rgba(238, 105, 105, 1)';
+
+}
+const playerNames = document.getElementsByClassName('player-name');
+for (let playerN of playerNames) {
+    playerN.style.color = 'white';
+
+}
+
+const playerBg = document.getElementsByClassName('player');
+for (let player of playerBg) {
+    player.style.backgroundColor = 'rgba(105, 109, 238, 1)';
+
+}
